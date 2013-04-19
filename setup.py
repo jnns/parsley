79368419c29c8a422247ef227f140e8da480c6e7
Jannis <jvajen@gmail.com>
2013-04-19 13:01:32 +0200

    Fix directory structure again.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name="Parsley",
     version="0.1",
-    packages=["parsley"],
+    py_modules=["parsley"],
     url="http://github.com/jnns/parsley", 
     license="GPL",
     long_description=open("README.rst").read(),
